009e2a0904c3744ca1c2fae789c047098d692297
AbhiCodes <chhettriabhishek9@gmail.com>
2025-04-13 15:20:08 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -3,13 +3,16 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import jobRoutes from "./routes/jobRoutes.js";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
