330d53a67f6e537031c2e079f7b38eac49744fa2
AbhiCodes <chhettriabhishek9@gmail.com>
2025-04-13 17:04:33 -0400

    added rate limit

--- server/server.js
+++ server/server.js
@@ -3,9 +3,16 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const jobRoutes = require("./routes/jobRoutes");
 const path = require("path");
-
+const rateLimit = require("express-rate-limit");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later.",
+  headers: true, // Send the rate limit info in the response headers
+});
+app.use(limiter);
 
 app.use(cors());
 app.use(express.json());
