af7df52331d43b78f78cae3848937f73585515c0
AbhiCodes <chhettriabhishek9@gmail.com>
2025-04-13 15:08:55 -0400

    second

--- server/server.js
+++ server/server.js
@@ -2,14 +2,22 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import jobRoutes from "./routes/jobRoutes.js";
+import path from "path";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 app.use("/jobs", jobRoutes);
 
-app.listen(3000, () => {
-  console.log("listening on 3000");
+app.listen(PORT, () => {
+  console.log(`running on ${PORT}`);
 });
